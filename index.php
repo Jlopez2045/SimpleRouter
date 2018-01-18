0095307571dedefd4c0cc03938d48774a43de275
Juan López Tello <juanlt2045@gmail.com>
2018-01-18 12:52:02 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php 
+require_once 'router/router.php';
+
+$router = new Router;
+
+$router->map("article/[i:articleid]/post/[s:slug]", function($request, $articleid, $slug){
+	echo "Article ID: $articleid, Slug of the Article: $slug";
+});
+
+$router->map("post/[i:year]/[i:month]", function($request, $year, $month){
+	echo 'post from date: ' . $year . '/' . $month;
+});
+
+$router->respond();
+?>
