94a78e4e68c8679e86ed4cabdafb441f7e9602d5
Juan López Tello <juanlt2045@gmail.com>
2018-01-23 19:58:07 -0500

    Add files via upload

--- index.php
+++ index.php
@@ -3,13 +3,18 @@ require_once 'router/router.php';
 
 $router = new Router;
 
-$router->map("article/[i:articleid]/post/[s:slug]", function($request, $articleid, $slug){
+$router->map("/home", function(){
+	echo "Homepage";
+});
+
+$router->map("/article/[a:articleid]/post/[s:slug]", function($request, $articleid, $slug){
 	echo "Article ID: $articleid, Slug of the Article: $slug";
 });
 
-$router->map("post/[i:year]/[i:month]", function($request, $year, $month){
+$router->map("/post/[i:year]/[i:month]", function($request, $year, $month){
 	echo 'post from date: ' . $year . '/' . $month;
 });
 
 $router->respond();
+
 ?>
